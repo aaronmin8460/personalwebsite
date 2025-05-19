99606c01c6394d2047a59a1799d17543e8532eed
Aaron Min <85600601+aaronmin8460@users.noreply.github.com>
2025-05-19 14:04:08 +0900

    10th

--- app/leetpost/two-sum/page.tsx
+++ app/leetpost/two-sum/page.tsx
@@ -3,14 +3,27 @@ import Image from 'next/image'
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
-const Pseudocode = `
-function twoSum(nums, target):
-    for i from 0 to length of nums - 1:
-        for j from i + 1 to length of nums - 1:
-            if nums[i] + nums[j] == target:
-                return [i, j]
-    return null (no pair found)
+const Pseudocode = `for i from 0 to n-1:
+    for j from i+1 to n:
+        if nums[i] + nums[j] == target:
+            return [i, j]
 `;
+
+const Code = `int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    for(int i = 0; i < numsSize; i++){
+        for(int j = i+1; j < numsSize; j++){
+            if(nums[j] + nums[i]== target){
+                int* result = malloc(2*sizeof(int));
+                result[0] = i;
+                result[1] = j;
+                *returnSize = 2;
+                return result;
+            }
+        }
+    }
+    *returnSize = 0;
+    return 0;
+}`;
 
 export default function TwoSome() {
 
@@ -26,13 +39,22 @@ export default function TwoSome() {
       alt="two-sum"
     />
     </div>
-    <h2 className="p-5 text-xl font-bold">Approach</h2>
+    <h2 className="pl-5 pr-5 text-xl font-bold">Approach</h2>
+    <p className = "p-5 font-semibold">using nested loop, compare and calculate each element in array without using same element twice. return the position of the element</p>
     <h2 className="pl-5 pr-5 w-auto text-xl font-bold">Pseudocode</h2>
     <div className="pl-5 pr-5">
       <SyntaxHighlighter language="text" style={atomDark}>
         {Pseudocode}
       </SyntaxHighlighter>
-          </div>
+      </div>
+    <h2 className="pl-5 pr-5 w-auto text-xl font-bold">Code in C</h2>
+      <div className="pl-5 pr-5">
+      <SyntaxHighlighter language="text" style={atomDark}>
+        {Code}
+      </SyntaxHighlighter>
+      </div>
+      <h2 className="pl-5 pr-5 text-xl font-bold">Time Complexity: O<sup>2</sup></h2>
+      <h2 className="pl-5 pr-5 text-xl font-bold">Space Complexity: O(1)</h2>
     </div>
   );
 }
