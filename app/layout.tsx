50a76693fad11797d33fbc32d98fc6ab0d56c37d
Aaron Min <85600601+aaronmin8460@users.noreply.github.com>
2025-05-19 12:46:37 +0900

    fifth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Navigation from "./components/navigation";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={`${inter.className} overflow-hidden`}>
         <Navigation />
         {children}
+        <Analytics />
       </body>
     </html>
   );
